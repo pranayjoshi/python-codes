803c82b4c026f1a1bd1330240b724a7a05855e76
Pranay Joshi <35572191+pranayteaches@users.noreply.github.com>
2020-05-11 21:03:15 +0530

    Create classes_staticmethod.py

---
+++ classes_staticmethod.py
@@ -0,0 +1,14 @@
+class Math:
+    @staticmethod
+    def add(a, b):
+        return a+b
+    @staticmethod
+    def sub(a,b):
+        return a-b
+    @staticmethod
+    def multi(a, b):
+        return a*b
+    @staticmethod
+    def div(a,b):
+        return a/b
+print(Math.add(12, 5))
